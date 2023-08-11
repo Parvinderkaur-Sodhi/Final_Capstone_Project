a640adcf53634f6621f81106f487c1b89a21a55e
Parvinderkaur-Sodhi <parvinder@sodhi.in>
2023-08-11 10:17:45 +0530

    Push changes to fecth

--- Frontend/src/services/Hr.service.js
+++ Frontend/src/services/Hr.service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL_EMP = "http://localhost:8081/api/employees";
-const API_URL_TYPE = "http://localhost:8081/api/leave-types";
-const API_URL_REQ = "http://localhost:8081/api/leave-requests";
-const API_URL_BAL = "http://localhost:8081/api/leave-balances";
+const API_URL_EMP = "http://localhost:8080/api/employees";
+const API_URL_TYPE = "http://localhost:8080/api/leave-types";
+const API_URL_REQ = "http://localhost:8080/api/leave-requests";
+const API_URL_BAL = "http://localhost:8080/api/leave-balances";
 
 class HrService {
 
