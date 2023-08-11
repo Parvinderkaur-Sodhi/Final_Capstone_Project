36478e49993c3c5f962c41e54b106258def654fd
raimadas <raimadas87@gmail.com>
2023-08-11 10:15:40 +0530

    commit 9

--- Frontend/src/services/employee.service.js
+++ Frontend/src/services/employee.service.js
@@ -9,19 +9,9 @@ class EmployeeService {
     return axios.get(`${API_URL}employees/${id}`, { headers: authHeader() });
   }
 
-<<<<<<< HEAD
-  getAttendanceByEmployeeId(employeeId){
-    return axios.get(`${API_URL}attendance/${employeeId}`);
-  }
-
-  getAllJobs(token){
-    console.log("dfdsf");
-    return axios.get(API_URL+'Jobs/viewJob',{headers:{"Authorization":`Bearer ${token}`}});
-=======
   getAllJobs(token){
     console.log("dfdsf");
      return axios.get(API_URL+'Jobs/viewJob',{headers:{"Authorization":`Bearer ${token}`}});
->>>>>>> 89edea8fe7e32bbccbbf5285319878cccf22646c
   }
   
 }
