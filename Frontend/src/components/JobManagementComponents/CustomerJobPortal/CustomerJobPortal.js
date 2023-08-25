fea2a2f5e0d0281f0cae74a399c92527ed8ddb53
Parvinderkaur-Sodhi <parvinder@sodhi.in>
2023-08-25 15:03:36 +0530

    Push final changes

--- Frontend/src/components/JobManagementComponents/CustomerJobPortal/CustomerJobPortal.js
+++ Frontend/src/components/JobManagementComponents/CustomerJobPortal/CustomerJobPortal.js
@@ -2,11 +2,7 @@ import React, { useState } from 'react'
 import FilterJob from './FilterJob'
 import SearchJob from './SearchJob'
 import AllJob from './AllJob'
-<<<<<<< HEAD
-import { Box, Button, Stack, Typography ,Card} from '@mui/material'
-=======
 import { Box, Button, Stack, Typography, Card } from '@mui/material'
->>>>>>> 7c57eeb3501863caa70e8882a8879ea2e3711ca3
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import FilterByCategory from './FilterByCategory'
 import { CardGiftcard } from '@mui/icons-material'
