cd54bb006be7c1354bcdc0eb85302aff82049fbc
Parvinderkaur-Sodhi <parvinder@sodhi.in>
2023-08-28 11:45:06 +0530

    App.js

--- Frontend/src/components/UserComponents/EmployeeHome.js
+++ Frontend/src/components/UserComponents/EmployeeHome.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { Card, CardContent, Typography, Grid } from "@mui/material";
 import { blue, green, red } from "@mui/material/colors";
-import EventIcon from "@mui/icons-material/Event"; 
+import EventIcon from "@mui/icons-material/Event";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
 import EmployeeNavbar from "../DashBoardComponents/EmployeeNavbar";
-
+import SmallCalendar from "../DashBoardComponents/SmallCalendar";
+import EmployeeService from "../../services/employee.service";
 
 function EmployeeHome(props) {
   const { user: currentUser } = props;
+
+  // State for leave requests and loading
+  const [leaveRequests, setLeaveRequests] = useState([]);
   const [employeeId, setEmployeeId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Loading state
 
   useEffect(() => {
     const storedEmployeeId = localStorage.getItem('employeeId');
@@ -18,6 +23,20 @@ function EmployeeHome(props) {
     }
   }, []);
 
+  useEffect(() => {
+    if (employeeId) {
+      EmployeeService.getLeaveRequestById(employeeId)
+        .then((response) => {
+          console.log(response.data); // Log the response data
+          setLeaveRequests(response.data.leaveRequests); // Use the array property
+          setIsLoading(false); // Data fetched, loading complete
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsLoading(false); // Error occurred, loading complete
+        });
+    }
+  }, [employeeId]);
 
   if (!currentUser) {
     return <Redirect to="/login" />;
@@ -25,95 +44,102 @@ function EmployeeHome(props) {
 
   return (
     <div>
-      <EmployeeNavbar/>
-      <Grid container spacing={3}>
+      <EmployeeNavbar />
+      <Card style={{ maxHeight: "80vh", overflowY: "auto", paddingRight: "17px", padding: "20px" }}>
+        <Grid container spacing={3}>
+          {/* Greeting */}
+          <Grid item xs={10}>
+            <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6" color="textPrimary">
+                  Hello!!!!! (Greetings)
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          {/* Saved Job Listings */}
+          <Grid item xs={3}>
+            <h2>Saved Job</h2>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6">Software Engineer</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  TechCo
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
 
-        {/* Greeting */}
-        <Grid item xs={10}>
-          <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6" color="textPrimary">
-                Hello!!!!! (Greetings)
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        {/* Saved Job Listings */}
-        <Grid item xs={3}>
-          <h2>Saved Job </h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6">Software Engineer</Typography>
-              <Typography variant="body1" color="textSecondary">
-                TechCo
-              </Typography>
-            </CardContent>
-          </Card>
-        
-        </Grid>
+          {/* Job Offers */}
+          <Grid item xs={3}>
+            <h2>Job Offers</h2>
+            <Card sx={{ backgroundColor: green[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6">Frontend Developer</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  WebDev
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
 
-        {/* Job Offers */}
-        <Grid item xs={3}>
-          <h2>Job Offers</h2>
-          <Card sx={{ backgroundColor: green[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6">Frontend Developer</Typography>
-              <Typography variant="body1" color="textSecondary">
-                WebDev
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+          <Grid item xs={3}>
+            <h2>Job Applied </h2>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6">Data Analyst</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  DataCorp
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          {/* Attendance Percentage */}
 
-        <Grid item xs={3}>
-          <h2>Job Applied </h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6">Data Analyst</Typography>
-              <Typography variant="body1" color="textSecondary">
-                DataCorp
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-         {/* Attendance Percentage */}
-         
-        <Grid item xs={5}>
-          <h2>Attendance</h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
-                <EventIcon style={{ marginRight: "8px" }} />
-                {/* Attendance Percentage: {attendancePercentage.toFixed(2)}% */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Present Days: {attendanceData.presentDays} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Total Working Days: {attendanceData.totalWorkingDays} */}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+          <Grid item xs={5}>
+            <h2>Attendance</h2>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
+                  <EventIcon style={{ marginRight: "8px" }} />
+                  {/* Attendance Percentage: {attendancePercentage.toFixed(2)}% */}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {/* Present Days: {attendanceData.presentDays} */}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {/* Total Working Days: {attendanceData.totalWorkingDays} */}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5}>
-          <h2>Leaves</h2>
-          <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
-                <LocalFloristIcon style={{ marginRight: "8px" }} />
-                {/* Leave Balance: {leaveData.totalLeaves - leaveData.usedLeaves} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Total Leaves: {leaveData.totalLeaves} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Used Leaves: {leaveData.usedLeaves} */}
-              </Typography>
-            </CardContent>
-          </Card>
+          {/* Leaves */}
+          <Grid item xs={5}>
+            <h2>Leaves</h2>
+            <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
+                  <LocalFloristIcon style={{ marginRight: "8px" }} />
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {/* ... (leave-related text) */}
+                </Typography>
+                {/* Conditional rendering based on loading state */}
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <div>
+                    {Array.isArray(leaveRequests) && (
+                      <SmallCalendar leaveRequests={leaveRequests} />
+                    )}
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </Card>
     </div>
   );
 }
--- Frontend/src/components/UserComponents/HrHome.js
+++ Frontend/src/components/UserComponents/HrHome.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
-import { Grid, Card, CardContent, Typography} from "@mui/material";
+import { Grid, Card, CardContent, Typography } from "@mui/material";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import HrService from "../../services/hr.service";
@@ -41,83 +41,86 @@ function HrHome(props) {
   return (
     <div>
       <HrNavbar />
-      <Card style={{ maxHeight: "80vh", overflowY: "auto", paddingRight: "17px", padding: "10px" }}>
-      <Grid container spacing={3}>
-        {/* Saved Job Listings */}
-        <Grid item xs={4}>
-          <h2>News</h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6">Software Engineer</Typography>
-              <Typography variant="body1" color="textSecondary">
-                TechCo
-              </Typography>
-            </CardContent>
-          </Card>
+      <Card style={{ maxHeight: "80vh", overflowY: "auto", paddingRight: "17px", padding: "14px" }}>
+        <Grid container spacing={3}>
+          {/* Saved Job Listings */}
+          <Grid item xs={4}>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6">Software Engineer</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  TechCo
+                </Typography>
+              </CardContent>
+            </Card>
 
-        </Grid>
+          </Grid>
 
-        {/* Job Offers */}
-        <Grid item xs={4}>
-          <h2>Job Offers</h2>
-          <Card sx={{ backgroundColor: green[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6">Frontend Developer</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+          {/* Job Offers */}
+          <Grid item xs={4}>
+            <Card sx={{ backgroundColor: green[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6">Frontend Developer</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  TechCo
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
 
-        <Grid item xs={4}>
-          <h2>Attendance</h2>
-          {/* <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
-            <CardContent>
-              <Box display="flex" justifyContent="center" alignItems="center">
+          <Grid item xs={4}>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+              <CardContent>
+                {/* <Box display="flex" justifyContent="center" alignItems="center">
                 <CircularProgress
                   variant="determinate"
                   value={attendancePercentage}
                   size={100}
                   thickness={6}
                 />
-                &nbsp;&nbsp;
-                <Typography variant="h6" gutterBottom>
+                </Box>
+                &nbsp;&nbsp; */}
+                <Typography variant="h6">
                   {attendancePercentage}%
                 </Typography>
-              </Box>
-            </CardContent>
-          </Card> */}
-        </Grid>
-        {/* Attendance Percentage */}
+                <Typography variant="body1" color="textSecondary">
+                  Attendance
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          {/* Attendance Percentage */}
 
-        <Grid item xs={6}>
-          <h2>Leaves</h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4, display: 'flex', justifyContent: 'center' }}>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Calendar
-              </Typography>
-              <SmallCalendar leaveRequests={leaveRequests} />
-            </CardContent>
-          </Card>
-        </Grid>
+          <Grid item xs={6}>
+            <h3>Leaves</h3>
+            <Card sx={{ backgroundColor: blue[100], marginBottom: 4, display: 'flex', justifyContent: 'center' }}>
+              <CardContent>
+                <Typography variant="h6" gutterBottom>
+                  Calendar
+                </Typography>
+                <SmallCalendar leaveRequests={leaveRequests} />
+              </CardContent>
+            </Card>
+          </Grid>
 
-        <Grid item xs={6}>
-          <h2>News and Events</h2>
-          <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
-                <EventIcon style={{ marginRight: "8px" }} />
-                {/* Attendance Percentage: {attendancePercentage.toFixed(2)}% */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Present Days: {attendanceData.presentDays} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Total Working Days: {attendanceData.totalWorkingDays} */}
-              </Typography>
-            </CardContent>
-          </Card>
+          <Grid item xs={6}>
+            <h3>News and Events</h3>
+            <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
+              <CardContent>
+                <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
+                  <EventIcon style={{ marginRight: "8px" }} />
+                  {/* Attendance Percentage: {attendancePercentage.toFixed(2)}% */}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {/* Present Days: {attendanceData.presentDays} */}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {/* Total Working Days: {attendanceData.totalWorkingDays} */}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
       </Card>
     </div>
   );
