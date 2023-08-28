c9cdb3d7096ee2a1e91d4145b14a11c49e6ee869
raimadas <raimadas87@gmail.com>
2023-08-28 11:51:10 +0530

    changes

--- Frontend/src/components/UserComponents/EmployeeHome.js
+++ Frontend/src/components/UserComponents/EmployeeHome.js
@@ -5,29 +5,79 @@ import { blue, green, red } from "@mui/material/colors";
 import EventIcon from "@mui/icons-material/Event"; 
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
 import EmployeeNavbar from "../DashBoardComponents/EmployeeNavbar";
+import EmployeeService from "../../services/employee.service";
+import { makeStyles } from "@mui/styles"; // Import makeStyles
+
+// function EmployeeHome(props) {
+//   const { user: currentUser } = props;
+//   const [employeeId, setEmployeeId] = useState(null);
+
+//   useEffect(() => {
+//     const storedEmployeeId = localStorage.getItem('employeeId');
+//     if (storedEmployeeId) {
+//       setEmployeeId(storedEmployeeId);
+//     }
+//   }, []);
 
 
+//   if (!currentUser) {
+//     return <Redirect to="/login" />;
+//   }
+
+const useStyles = makeStyles((theme) => ({
+  attendanceCard: {
+    backgroundColor: blue[100],
+    padding: 16, // Use numeric value instead of theme.spacing
+    borderRadius: 8, // Use numeric value instead of theme.spacing
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+  attendanceStatus: {
+    color: "#fff",
+    backgroundColor: (props) =>
+      props.attendanceStatus === "Poor" ? "red" : "green",
+    padding: 8, // Use numeric value instead of theme.spacing
+    borderRadius: 8, // Use numeric value instead of theme.spacing
+    display: "inline-block",
+  },
+}));
 
 function EmployeeHome(props) {
   const { user: currentUser } = props;
-  const [employeeId, setEmployeeId] = useState(null);
+  const [attendancePercentage, setAttendancePercentage] = useState(0);
+  const [attendanceStatus, setAttendanceStatus] = useState(""); // Default status
+  const employeeId = localStorage.getItem("employeeId");
+  const classes = useStyles({ attendanceStatus }); 
 
   useEffect(() => {
-    const storedEmployeeId = localStorage.getItem('employeeId');
-    if (storedEmployeeId) {
-      setEmployeeId(storedEmployeeId);
-    }
-  }, []);
+    EmployeeService.getAttendanceByEmployeeId(employeeId)
+      .then((response) => {
+        const presentDays = response.data.filter(
+          (record) => record.present.toLowerCase() === "present"
+        ).length;
+        const percentage = (presentDays / response.data.length) * 100 || 0;
+        setAttendancePercentage(percentage);
 
+        // Determine attendance status
+        if (percentage < 50) {
+          setAttendanceStatus("Poor");
+        } else {
+          setAttendanceStatus("Good");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [employeeId]);
 
   if (!currentUser) {
     return <Redirect to="/login" />;
   }
 
+
   return (
     <div>
       <EmployeeNavbar/>
-      <Card style={{ padding: 20 }}>
+      <Card style={{ padding: 5 }}>
 
       <Grid container spacing={3}>
 
@@ -81,37 +131,21 @@ function EmployeeHome(props) {
         </Grid>
          {/* Attendance Percentage */}
          
-        <Grid item xs={5}>
-          <h2>Attendance</h2>
-          <Card sx={{ backgroundColor: blue[100], marginBottom: 4 }}>
+        
+         <Grid item xs={12} sm={6}>
+          <Card className={classes.attendanceCard}>
             <CardContent>
-              <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
-                <EventIcon style={{ marginRight: "8px" }} />
-                {/* Attendance Percentage: {attendancePercentage.toFixed(2)}% */}
+              <Typography variant="h5" style={{ marginBottom: "16px" }}>
+                <strong>Your Attendance Details</strong>
               </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Present Days: {attendanceData.presentDays} */}
+              <Typography variant="h6" style={{ marginBottom: "8px" }}>
+                Attendance Percentage: {attendancePercentage.toFixed(2)}%
               </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Total Working Days: {attendanceData.totalWorkingDays} */}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={5}>
-          <h2>Leaves</h2>
-          <Card sx={{ backgroundColor: red[100], marginBottom: 4 }}>
-            <CardContent>
-              <Typography variant="h6" style={{ display: "flex", alignItems: "center" }}>
-                <LocalFloristIcon style={{ marginRight: "8px" }} />
-                {/* Leave Balance: {leaveData.totalLeaves - leaveData.usedLeaves} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Total Leaves: {leaveData.totalLeaves} */}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                {/* Used Leaves: {leaveData.usedLeaves} */}
+              <Typography variant="subtitle1">
+                Attendance Status:{" "}
+                <span className={classes.attendanceStatus}>
+                  {attendanceStatus}
+                </span>
               </Typography>
             </CardContent>
           </Card>
